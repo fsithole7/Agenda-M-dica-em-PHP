fa83893ac70ad8393fa10890ec5edabae4066280
fsithole7 <101812958+fsithole7@users.noreply.github.com>
2023-05-17 18:05:25 +0200

    Add files via upload

---
+++ admin/navbar.php
@@ -0,0 +1,23 @@
+
+<style>
+</style>
+<nav id="sidebar" class='mx-lt-5 bg-dark' >
+		
+		<div class="sidebar-list">
+
+				<a href="index.php?page=home" class="nav-item nav-home"><span class='icon-field'><i class="fa fa-home"></i></span> Medicos</a>
+				<a href="index.php?page=categories" class="nav-item nav-categories"><span class='icon-field'><i class="fa fa-book-medical"></i></span> Especialidades</a>			
+				 
+		</div>
+
+</nav>
+<script>
+	$('.nav-<?php echo isset($_GET['page']) ? $_GET['page'] : '' ?>').addClass('active')
+</script>
+<?php if($_SESSION['login_type'] == 2): ?>
+	<style>
+		.nav-sales ,.nav-users,.nav-doctors,.nav-categories{
+			display: none!important;
+		}
+	</style>
+<?php endif ?>
